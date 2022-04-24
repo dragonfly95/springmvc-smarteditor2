b53347b46a8b813ae0e2b80beb7cb673b0a40feb
dragonfly95 <nosignal95@gmail.com>
2022-04-25 08:21:00 +0900

    프로젝트 세팅

--- src/main/webapp/blog/smarteditor2/popup/photo_uploader/attach_photo.js
+++ src/main/webapp/blog/smarteditor2/popup/photo_uploader/attach_photo.js
@@ -338,7 +338,7 @@
     	//sUploadURL= 'file_uploader_html5.php'; 	//upload URL
     	//sUploadURL= 'file_uploader_html5.jsp'; 	//upload URL
 
-    	sUploadURL= '/api/shop/file/brandstory-multiImgUpload?TOKEN='+sessionStorage['TOKEN'];
+    	sUploadURL= '/common/file-upload';
 
     	//파일을 하나씩 보내고, 결과를 받음.
     	for(var j=0, k=0; j < nImageInfoCnt; j++) {
