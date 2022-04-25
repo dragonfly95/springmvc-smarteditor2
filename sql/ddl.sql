6d6117346e05be3bac76f8aac77b5f0884a9c237
dragonfly95 <nosignal95@gmail.com>
2022-04-25 11:56:30 +0900

    스마트에디터

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -3,7 +3,7 @@
 -- -----------------------------------------------------
 -- Table `mydb`.`User`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `User` (
+CREATE TABLE IF NOT EXISTS `user` (
   `id` VARCHAR(45) NOT NULL,
   `name` VARCHAR(45) NULL,
   `passwd` VARCHAR(45) NULL,
@@ -19,12 +19,7 @@ CREATE TABLE IF NOT EXISTS `category` (
   `id` VARCHAR(45) NOT NULL,
   `name` VARCHAR(45) NULL,
   `user_id` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `user_id`
-    FOREIGN KEY (`id`)
-    REFERENCES `mydb`.`User` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
@@ -38,19 +33,7 @@ CREATE TABLE IF NOT EXISTS `Post` (
   `content` TEXT NULL,
   `user_id` VARCHAR(45) NULL,
   `reg_date` TIMESTAMP NULL,
-  PRIMARY KEY (`id`),
-  INDEX `category_id_idx` (`category_id` ASC) VISIBLE,
-  INDEX `user_id_idx` (`user_id` ASC) VISIBLE,
-  CONSTRAINT `category_id`
-    FOREIGN KEY (`category_id`)
-    REFERENCES `mydb`.`category` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `user_id`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `mydb`.`User` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
@@ -60,18 +43,12 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `comment` (
   `id` VARCHAR(45) NOT NULL,
   `comment_text` VARCHAR(3000) NULL,
-  `reg_date` DATETIME NULL,
+  `reg_date` TIMESTAMP NULL,
   `post_id` VARCHAR(45) NULL,
   `user_id` VARCHAR(45) NULL,
-  `reg_date` TIMESTAMP NULL,
-  PRIMARY KEY (`id`),
-  INDEX `post_id_idx` (`post_id` ASC) VISIBLE,
-  CONSTRAINT `post_id`
-    FOREIGN KEY (`post_id`)
-    REFERENCES `mydb`.`Post` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
+
 
 
 
