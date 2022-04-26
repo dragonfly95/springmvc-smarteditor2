82da1ece3eb4d3705d3956c7d95b2fb7f48f6d85
dragonfly95 <nosignal95@gmail.com>
2022-04-26 10:42:45 +0900

    스마트에디터

--- src/main/java/com/system/blog/post/CommentController.java
+++ src/main/java/com/system/blog/post/CommentController.java
@@ -3,6 +3,7 @@ package com.system.blog.post;
 import com.system.blog.ResponseVO;
 import com.system.blog.post.mapper.CommentMapper;
 import com.system.blog.post.vo.CommentVO;
+import egovframework.rte.psl.dataaccess.util.EgovMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping(value = "comment")
 public class CommentController {
@@ -22,16 +25,23 @@ public class CommentController {
 
     @GetMapping("list/{postId}")
     private ResponseEntity list(@PathVariable("postId") String postId) {
-        return ResponseEntity.ok().body(ResponseVO.of("ok"));
+        List<CommentVO> list = commentMapper.list(postId);
+
+        return ResponseEntity.ok().body(list);
     }
 
     @PostMapping("insertProcess/{postId}")
     private ResponseEntity insertProcess(@RequestBody CommentVO commentVO) {
+        int row = commentMapper.insertProcess(commentVO);
         return ResponseEntity.ok().body(ResponseVO.of("ok"));
     }
 
     @DeleteMapping("deleteProcess/{postId}/{commentId}")
     private ResponseEntity deleteProcess(@PathVariable("postId") String postId, @PathVariable("commentId") String commentId) {
+        EgovMap map = new EgovMap();
+        map.put("postId", postId);
+        map.put("commentId", commentId);
+        int row = commentMapper.deleteProcess(map);
         return ResponseEntity.ok().body(ResponseVO.of("ok"));
     }
 
